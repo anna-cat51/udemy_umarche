28c3fe4522a098d6f9e54ac46861ae12f9530429
AnnaDoi <willow6510@yahoo.co.jp>
2024-10-23 11:57:17 +0900

    image,store

--- app/Services/ImageService.php
+++ app/Services/ImageService.php
@@ -9,13 +9,20 @@ use Intervention\Image\Drivers\Gd\Driver;
 class ImageService
 {
   public static function upload($imageFile, $folderName){
+
+    if(is_array($imageFile))
+    {
+      $file = $imageFile['image'];
+    }else{
+      $file = $imageFile;
+    }
     $manager = new ImageManager(new Driver());
     // 画像を読み込む
-    $image = $manager->read($imageFile->getPathname());
+    $image = $manager->read($file->getPathname());
     // 重複しないファイル名をつくる
     $fileName = uniqid(rand().'_');
     // 拡張子を取得
-    $extension = $imageFile->extension();
+    $extension = $file->extension();
     // 上記のファイル名と拡張子を合体
     $fileNameToStore = $fileName. '.' .$extension;
 
