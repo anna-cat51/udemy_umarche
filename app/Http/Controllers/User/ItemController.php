d152c29968741681621b699510c2ce067e399f23
AnnaDoi <willow6510@yahoo.co.jp>
2024-10-28 17:17:25 +0900

    検索フォーム カテゴリー

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -32,11 +32,11 @@ class ItemController extends Controller
 
     public function index(Request $request)
     {
-        dd($request);
         $categories = PrimaryCategory::with('secondary')
         ->get();
 
         $products = Product::availableItems()
+        ->selectCategory($request->category ?? '0')
         ->sortOrder($request->sort)
         ->paginate($request->pagination ?? '20');
 
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -7,11 +7,11 @@
                     <div class="lg:flex lg:justify-around">
                         <div class="lg:flex items-center">
                             <select name="category" class="mb-2 lg:mb-0 lg:mr-2" id="">
-                                <option value="0">全て</option>
+                                <option value="0" @if(\Request::get('category') === '0') selected @endif>全て</option>
                                 @foreach($categories as $category)
                                 <optgroup label="{{ $category->name }}">
                                     @foreach($category->secondary as $secondary)
-                                <option value="{{ $secondary->id}}" >
+                                <option value="{{ $secondary->id}}" @if(\Request::get('category') == $secondary->id) selected @endif>
                                     {{ $secondary->name }}
                                 </option>
                                 @endforeach
@@ -97,7 +97,8 @@
                     </div>
                     {{ $products->appends([
                         'sort' => \Request::get('sott'),
-                        'pagination' => \Request::get('pagination')
+                        'pagination' => \Request::get('pagination'),
+                        'category' => \Request::get('category')
                     ])->links() }}
                 </div>
             </div>
