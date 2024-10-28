9d578cff328b9ac8007bab8a4b216ccb8cd1c907
AnnaDoi <willow6510@yahoo.co.jp>
2024-10-28 10:59:31 +0900

    cart view

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -59,6 +59,8 @@
                         <div>
                           <span class="title-font font-medium text-2xl text-gray-900">{{ number_format($product->price) }} <span class="text-sm text-gray-700">円(税込)</span></span>
                         </div>
+                        <form method="post" action="{{ route('user.cart.add') }}">
+                          @csrf
                         <div class="flex items-center">
                           <span class="mr-3">数量</span>
                           <div class="relative">
@@ -70,6 +72,8 @@
                         </div>
                         </div>
                         <button class="flex ml-auto text-white bg-yellow-500 border-0 py-2 px-6 focus:outline-none hover:bg-yellow-600 rounded">カートに入れる</button>
+                      <input type="hidden" name="puroduct_id" value="{{  $product->id }}">
+                      </form>
                       </div>
                     </div>
                   </div>
