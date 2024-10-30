1f0a7d33f22470e3bf21301eb7e0df7078e49087
AnnaDoi <willow6510@yahoo.co.jp>
2024-10-30 12:04:13 +0900

    ユーザー向けメール送信

--- app/Jobs/SendThanksMail.php
+++ app/Jobs/SendThanksMail.php
@@ -9,21 +9,19 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Mail;
-use App\Mail\TestMail;
-
+use App\Mail\ThanksMail;
 
 class SendThanksMail implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    /**
-     * Create a new job instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    public $products;
+    public $user;
+
+    public function __construct($products, $user)
     {
-        //
+        $this->products = $products;
+        $this->user = $user;
     }
 
     /**
@@ -33,7 +31,7 @@ class SendThanksMail implements ShouldQueue
      */
     public function handle()
     {
-        mail::to('test@example.com')
-        ->send(new TestMail());
+        Mail::to($this->user)
+        ->send(new ThanksMail($this->products, $this->user));
     }
 }
--- app/Mail/ThanksMail.php
+++ app/Mail/ThanksMail.php
@@ -11,14 +11,13 @@ class ThanksMail extends Mailable
 {
     use Queueable, SerializesModels;
 
-    /**
-     * Create a new message instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    public $products;
+    public $user;
+
+    public function __construct($products, $user)
     {
-        //
+        $this->products = $products;
+        $this->user = $user;
     }
 
     /**
@@ -28,6 +27,7 @@ class ThanksMail extends Mailable
      */
     public function build()
     {
-        return $this->view('view.name');
+        return $this->view('emails.thanks')
+        ->subject('ご購入ありがとうございます。');
     }
 }
